8eb51b96191af16e361af038c5f4086b6969c9eb
Jeff Holm <jholm117@gmail.com>
2018-05-20 23:05:48 -0500

    framework update

--- scripts/homework.sh
+++ scripts/homework.sh
@@ -13,30 +13,33 @@ cd ../$dirName ;
 cp -r "${origDir}"/* ./ ;
 
 # Remove unnecessary files
-rm -r lib Makefile scripts bin ;
+rm -f */* &> /dev/null ;
+rm -rf lib Makefile scripts bin ;
 rm -r */tests ;
 rm -fr */obj ;
 rm -fr */bin ;
 rm -r */scripts ;
 rm -rf `find ./ -name .DS_Store`
-rm  */Makefile ;
 rm -f `find ./ -iname *.swp`
 for i in `ls` ; do
   if ! test -d $i ; then
     continue ;
   fi
+
   pushd ./ ;
   cd $i ;
   for j in `ls` ; do
-    if test -d $j ; then
+    if test -d $j -a "$j" == "src" ; then
       continue ;
     fi
-    rm -f $j ;
+    rm -rf $j ;
   done
   popd ;
 done
+find ./ -empty -type d -delete ;
 
 # Create the package
+echo "SIGNATURE = 4903ufjdsk:" > signature.txt ;
 tar cfj ../${dirName}.tar.bz2 ./ ;
 cd ../ ;
 mv ${dirName}.tar.bz2 "${origDir}"/ ;
